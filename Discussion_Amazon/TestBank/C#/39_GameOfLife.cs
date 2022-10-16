7c96b739a9f09bf6aea3988bcc1a84800e3e9b49
jeffreyh93 <43526518+jeffreyh93@users.noreply.github.com>
2022-10-16 13:56:14 -0700

    Completed up to and including question 40

--- Discussion_Amazon/TestBank/C#/39_GameOfLife.cs
+++ Discussion_Amazon/TestBank/C#/39_GameOfLife.cs
@@ -14,6 +14,50 @@ The next state is created by applying the above rules simultaneously to every ce
 
 public class Solution {
     public void GameOfLife(int[][] board) {
-        
+        int m = board.Length, n = board[0].Length;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                board[i][j] = CheckAdjacent(board, i, j, m, n);
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == -1) board[i][j] = 0;
+                else if (board[i][j] == 2) board[i][j] = 1;
+            }
+        }
+    }
+
+    public int CheckAdjacent(int[][] board, int r, int c, int m, int n) {
+        int[] rDir = {1, 0, -1, 0, 1, 1, -1, -1};
+        int[] cDir = {0, 1, 0, -1, 1, -1, 1, -1};
+
+        int liveAdj = 0;
+        for (int i = 0; i < rDir.Length; i++) {
+            if (r + rDir[i] == m || r + rDir[i] < 0 || c + cDir[i] == n || c + cDir[i] < 0) continue;
+            else {
+                if (Math.Abs(board[r + rDir[i]][c + cDir[i]]) == 1) liveAdj++;
+            }
+        }
+
+        if (board[r][c] == 1) {
+            if (liveAdj < 2) return -1;
+            else if (liveAdj < 4) return 1;
+            else if (liveAdj > 3) return -1;
+        }
+        else {
+            if (liveAdj == 3) return 2;
+        }
+        return 0;
     }
 }
+
+/**
+Algorithm: Arrays and Hashing
+Idea is that for every cell we need to check all directions. Then check the magnitudes of the adjacent cells, if the magnitude == 1 then it was alive before the next year. Then when we know the adjacent cell alive count during the first year, assign according to the conditions. If the cell was alive -> dead then assign it a -1 because we can check magnitude on future checks, if cell was dead -> alive assign it a 2 because we don't need to check on > 1s. Then on final iteration assign cells < 0 to be 0 and cells >= 1 to be 1.
+
+Space: O(1), no extra space used
+Time: O(m x n), all cells are iterated through
+*/
